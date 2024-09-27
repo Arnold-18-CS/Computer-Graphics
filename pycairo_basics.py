76f39d61818f8cb21a3c73f58b62abca561c7c2e
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-09-27 07:38:31 +0300

    Setting up a surface and drawing a basic image

---
+++ pycairo_basics.py
@@ -0,0 +1,24 @@
+import cairo
+
+# Setting up a surface - first step
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 600) 
+# PARAMETERS: output type (ImageSurface == png)
+# ,format, width, length
+
+# Setup a brush aka context
+context = cairo.Context(surface)
+# takes the surface/canvas as argument
+context.set_source_rgb(0.8, 0.8, 0.8)
+# pick color of surface with rgb values
+context.paint()
+# set paint color on surface
+
+# Draw an image
+context.rectangle(50,50,100,240)
+# give coordinates and dimensions
+context.set_source_rgb(1,0,0)
+# pick color of rectangle with rgb values
+context.fill()
+# set paint to fill rectangle
+
+surface.write_to_png('basics_output.png')
