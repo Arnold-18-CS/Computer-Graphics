611891ed63ce0777d794760c80d467aa1a658087
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-09-27 08:36:11 +0300

    Trial to create personal polygons

---
+++ polygon.py
@@ -0,0 +1,34 @@
+import cairo 
+
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 600)
+ctx = cairo.Context(surface)
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.paint()
+
+coordinates = []
+points = int(input("vertices: "))
+og_points = points
+
+while(points>0):
+    x = int(input("x: "))
+    y = int(input("y: "))
+    print()
+    A = [x,y]
+    coordinates.append(A)
+    points-=1
+
+# print(coordinates)
+
+ctx.move_to(*coordinates[0])
+while(og_points-1 > 0):
+    i = 1
+    ctx.line_to(*coordinates[i])
+    i+=1
+    og_points-=1
+
+ctx.close_path()
+ctx.set_source_rgb(0,0,0)
+ctx.set_line_width(5)
+ctx.stroke()
+
+surface.write_to_png("polygon.png")
